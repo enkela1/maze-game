4108de810bc7b42721d33428f0144c9a6bc8ca15
enkela1 <tafaenkela1@gmail.com>
2025-01-19 23:45:46 +0100

    Updated Map and nr of Coins Collected

---
+++ assets/TiledSmall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="TiledSmall" tilewidth="8" tileheight="8" tilecount="2500" columns="50">
+ <image source="Tiles.png" width="400" height="400"/>
+</tileset>
